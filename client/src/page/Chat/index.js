3d0e2b9fcf753fb3d729bfd2020eea83132dd0bd
Jared <kuil5230@gmail.com>
2018-02-02 21:37:38 +0800

    ci

--- client/src/page/Chat/index.js
+++ client/src/page/Chat/index.js
@@ -8,41 +8,12 @@ class ChatRoom extends Component {
   state = {
     chats: [
       {
-        username: "Kevin Hsu",
-        content: <p>你好</p>
+        username: "user",
+        content: "hello"
       },
       {
-        username: "Alice Chen",
-        content: <p>Love it!</p>
-      },
-      {
-        username: "Kevin Hsu",
-        content: <p>Check out my Github at</p>
-      },
-      {
-        username: "KevHs",
-        content: (
-          <p>
-            Lorem ipsum dolor sit amet, nibh ipsum. Cum class sem inceptos incidunt sed sed. Tempus wisi enim
-            id, arcu sed lectus aliquam, nulla vitae est bibendum molestie elit risus.
-          </p>
-        )
-      },
-      {
-        username: "Kevin Hsu",
-        content: <p>So</p>
-      },
-      {
-        username: "Kevin Hsu",
-        content: <p>Chilltime is going to be an app for you to view videos with friends</p>
-      },
-      {
-        username: "Kevin Hsu",
-        content: <p>You can sign-up now to try out our private beta!</p>
-      },
-      {
-        username: "Alice Chen",
-        content: <p>Definitely! Sounds great!</p>
+        username: "ChatBot",
+        content: "hey!"
       }
     ]
   };
@@ -65,28 +36,26 @@ class ChatRoom extends Component {
       {
         chats: this.state.chats.concat([
           {
-            username: "Kevin Hsu",
-            content: <p>{ReactDOM.findDOMNode(this.refs.msg).value}</p>
+            username: "user",
+            content: <p>{ReactDOM.findDOMNode(this.refs.message).value}</p>
           }
         ])
       },
       () => {
-        ReactDOM.findDOMNode(this.refs.msg).value = "";
+        ReactDOM.findDOMNode(this.refs.message).value = "";
       }
     );
   }
 
   render() {
-    const username = "Kevin Hsu";
-    const { chats } = this.state;
     return (
       <div className="ChatRoom">
         <h4>聊天室</h4>
         <ul className="chats" ref="chats">
-          {chats.map(chat => <HandleMessage chat={chat} user={username} />)}
+          {this.state.chats.map((chat, index) => <HandleMessage key={index} chat={chat} user={"user"} />)}
         </ul>
         <form className="input" onSubmit={e => this.submitMessage(e)}>
-          <input type="text" ref="msg" />
+          <input type="text" ref="message" />
           <input type="submit" value={"送出"} />
         </form>
       </div>
