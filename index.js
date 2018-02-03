8e6e5563f9c483d45261b32babcf1ebfa0cb9a9c
Jared <kuil5230@gmail.com>
2018-02-03 15:29:07 +0800

    ci

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const app = require("express")();
+const express = require("express");
 const path = require("path");
 //載入壓縮組件
 const compression = require("compression");
 //載入路由配置
 const routes = require("./server/route");
 
+const app = express();
 //socket.io config
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
 app.use(compression());
+app.use(express.static(path.join(__dirname, "client/build")));
 routes(app);
 
-server.listen(process.env.PORT || 5000, () => {
-  console.log("runing on http://localhost:5000 ");
-});
+server.listen(process.env.PORT || 5000);
 
 //其他頁面導向index
 app.get("*", (req, res) => {
@@ -22,7 +22,7 @@ app.get("*", (req, res) => {
 });
 
 io.on("connection", socket => {
-  socket.emit("ChatMessage", { server: "response mes to client" });
+  socket.emit("ChatMessage", { server: "response msg to client" });
   socket.on("my client", data => {
     console.log("server socket.on", data);
   });
