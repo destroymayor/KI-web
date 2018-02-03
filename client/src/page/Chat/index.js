fa62916fcc451128bf2b890becc01356157c762e
Jared <kuil5230@gmail.com>
2018-02-03 16:54:18 +0800

    ci

--- client/src/page/Chat/index.js
+++ client/src/page/Chat/index.js
@@ -4,28 +4,34 @@ import "./index.css";
 
 import HandleMessage from "./handleMessage";
 
+//載入socket.io client庫
 import io from "socket.io-client";
-const socket = io().connect("http://localhost:3000/");
+const socket = io.connect("http://localhost:3000/");
 
 class ChatRoom extends Component {
   state = {
     chats: [
       {
-        username: "user",
-        content: "hello"
-      },
-      {
         username: "ChatBot",
-        content: "hey!"
+        content: "嗨"
       }
     ]
   };
 
   componentDidMount() {
     this.scrollToBot();
-    socket.on("ChatMessage", data => {
-      console.log("client message", data);
-      socket.emit("my client", { client: "client msg to server" });
+
+    //接收server回傳的訊息
+    socket.on("SendMessage", data => {
+      this.setState({
+        chats: this.state.chats.concat([
+          {
+            username: "ChatBot",
+            content: data.content
+          }
+        ])
+      });
+      console.log("client NewMessage", data);
     });
   }
 
@@ -39,12 +45,18 @@ class ChatRoom extends Component {
 
   submitMessage(e) {
     e.preventDefault();
+
+    if (ReactDOM.findDOMNode(this.refs.message).value === "") return null;
+
+    //傳送訊息
+    socket.emit("SendMessage", { username: "User", content: ReactDOM.findDOMNode(this.refs.message).value });
+
     this.setState(
       {
         chats: this.state.chats.concat([
           {
             username: "user",
-            content: <p>{ReactDOM.findDOMNode(this.refs.message).value}</p>
+            content: ReactDOM.findDOMNode(this.refs.message).value
           }
         ])
       },
--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 const express = require("express");
+const app = express();
 const path = require("path");
+
 //載入壓縮組件
 const compression = require("compression");
 //載入路由配置
 const routes = require("./server/route");
 
-const app = express();
-//socket.io config
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
-
 app.use(compression());
 app.use(express.static(path.join(__dirname, "client/build")));
 routes(app);
-
-server.listen(process.env.PORT || 5000);
 
 //其他頁面導向index
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-io.on("connection", socket => {
-  socket.emit("ChatMessage", { server: "response msg to client" });
-  socket.on("my client", data => {
-    console.log("server socket.on", data);
+//socket.io config
+const server = require("http").Server(app);
+const io = require("socket.io").listen(server);
+//socket.io connection
+io.sockets.on("connection", socket => {
+  //接收client訊息並回傳
+  socket.on("SendMessage", data => {
+    console.log("server接收訊息", "使用者:", data.username, "內容:", data.content);
+    socket.emit("SendMessage", { username: "ChatBot", content: "server回傳訊息" });
   });
 });
+
+server.listen(process.env.PORT || 5000);
