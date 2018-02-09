0ca070540f00d64a15df5083f1aa496cb4aa7264
Jared <kuil5230@gmail.com>
2018-02-10 00:54:32 +0800

    add 404 page

---
+++ client/src/page/Notfound/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./index.css";
+
+class Notfound extends Component {
+  render() {
+    return (
+      <div className="Notfound">
+        <h1>您訪問的頁面不存在</h1>
+      </div>
+    );
+  }
+}
+
+export default Notfound;
