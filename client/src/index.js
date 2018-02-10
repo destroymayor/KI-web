cb632a7817c2fafe0fc0d4d910410758caba5462
Jared <kuil5230@gmail.com>
2018-02-10 14:20:03 +0800

    ci

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { unregister as unregisterServiceWorker } from "./registerServiceWorker";
+//import {  unregister as unregisterServiceWorker } from "./registerServiceWorker";
+import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 //載入路由配置
 import App from "./page/index";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-unregisterServiceWorker();
+registerServiceWorker();
+//unregisterServiceWorker();
