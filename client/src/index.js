c012428f52ddb6afedb71f9c7a056f88970e1db1
Jared <kuil5230@gmail.com>
2018-02-28 23:47:14 +0800

    ci

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { unregister as unregisterServiceWorker } from './registerServiceWorker';
-import registerServiceWorker from "./registerServiceWorker";
+import { unregister as unregisterServiceWorker } from './registerServiceWorker';
+// import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 
 import App from "./page/index";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-registerServiceWorker();
-// unregisterServiceWorker();
+// registerServiceWorker();
+unregisterServiceWorker();
