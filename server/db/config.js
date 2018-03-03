c027acac775b0ab64fa260663399c8db702bea2f
Jared <kuil5230@gmail.com>
2018-03-03 22:39:58 +0800

    ci

--- server/db/config.js
+++ server/db/config.js
@@ -7,17 +7,27 @@ const pool = mysql.createPool({
   database: 'chain_sea',
   port: 3306,
   connectionLimit: 30,
+  connectTimeout: 20000,
+  acquireTimeout: 20000,
 });
 
 const Query = sql =>
   new Promise((resolve, reject) => {
     pool.getConnection((err, connection) => {
-      if (err) return reject(err);
+      if (err) {
+        console.log('pool getConnection error');
+        return reject(err);
+      }
 
       return connection.query(sql, (error, rows) => {
-        if (error) return reject(error);
-        resolve(rows);
+        // 釋放連接
         connection.release();
+        if (error) {
+          console.log('query error', error);
+          return reject(error);
+        }
+
+        resolve(rows);
 
         return console.log(` \nConnection closed => ${new Date().toLocaleString()} \n `);
       });
